4c6a28080dccf6ab52af364bff86ce06e2f84561
przmnovitas <Not Set>
2021-08-22 21:01:14 +0200

    Declare initial variables to store information of the books.

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,20 @@
 public class Library {
+    public static void main(String[] args) {
+        final String appName = "Library v0.1";
+
+        String title = "";
+        String author = "";
+        int publishDate = 0;
+        int pagesNumber = 0;
+        String publisher = "";
+        String isbn = "";
+
+        System.out.println(appName);
+        System.out.println("Books available in the Library:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(publishDate);
+        System.out.println(pagesNumber);
+        System.out.println(isbn);
+    }
 }
