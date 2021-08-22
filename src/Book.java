f864965e2360759523e1f03a5120d0c9196bd415
przmnovitas <Not Set>
2021-08-22 21:27:55 +0200

    Add constructor into the Book class and printBookInformations() function  to display informations according to the stored book.

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,4 @@
-public class Book {
+class Book {
     // This class is a template for creating new book in the future.
     String title;
     String author;
@@ -6,4 +6,20 @@ public class Book {
     int pagesNumber;
     String publisher;
     String isbn;
+
+    // Constructor allows me to create new instances of the Book type.
+    Book(String title, String author, int publishDate, int pagesNumber, String publisher, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.publishDate = publishDate;
+        this.pagesNumber = pagesNumber;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
+
+    void printBookInformation() {
+        String information = title + "; " + author + "; " + publishDate + "; " + pagesNumber
+                + "; " + publisher + "; " + isbn + ".";
+        System.out.println(information);
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,13 @@
 public class Library {
     // This class will store information about books according to the Book template.
     public static void main(String[] args) {
-        final String appName = "Library v0.2";
-
-        Book book_1 = new Book();
-        book_1.title = "";
-        book_1.author = "";
-        book_1.publishDate = 0;
-        book_1.pagesNumber = 0;
-        book_1.publisher = "";
-        book_1.isbn = "";
+        final String appName = "Library v0.3";
 
         System.out.println(appName);
         System.out.println("Books available in the Library:");
-        System.out.println(book_1.title);
-        System.out.println(book_1.author);
-        System.out.println(book_1.publishDate);
-        System.out.println(book_1.pagesNumber);
-        System.out.println(book_1.isbn);
+        Book book_1 = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz", 1997, 330,
+                "Historia", "dbsjcbd2342");
+        book_1.printBookInformation();
+
     }
 }
