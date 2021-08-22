bb28e8d7e556cd0d49cc777652ffb8cc2f1064ba
przmnovitas <Not Set>
2021-08-22 22:01:26 +0200

    Add overloaded constructors into the Book class.

--- src/Book.java
+++ src/Book.java
@@ -7,17 +7,41 @@ class Book {
     String publisher;
     String isbn;
 
-    // Constructor allows me to create new instances of the Book type.
-    Book(String title, String author, int publishDate, int pagesNumber, String publisher, String isbn) {
+    /*
+    Constructor allows me to create new instances of the Book type.
+    If there are more than one constructors in the same class - overloaded constructors.
+    Overloaded constructors (assumptions):
+    - different numbers of the parameters;
+    - in the case fo the same number of the parameters, the types should be different.
+    A keyword 'this' allows me to refer to the class's components.
+     */
+
+    // overloaded constructor
+    public Book(String title, String author, int publishDate, int pagesNumber, String publisher, String isbn) {
+        this(title, author, publishDate, pagesNumber, publisher);
+        this.isbn = isbn; // class's field = parameter
+    }
+
+    // overloaded constructor
+    Book(String title, String author, int publishDate, int pagesNumber, String publisher) {
+        this(title, author, publishDate, pagesNumber);
+        this.publisher = publisher;
+    }
+
+    // overloaded constructor
+    public Book(String title, String author, int publishDate, int pagesNumber) {
+        this(title, author, publishDate);
+        this.pagesNumber = pagesNumber;
+    }
+
+    // Default constructor - the information below are mandatory
+    public Book(String title, String author, int publishDate) {
         this.title = title;
         this.author = author;
         this.publishDate = publishDate;
-        this.pagesNumber = pagesNumber;
-        this.publisher = publisher;
-        this.isbn = isbn;
     }
 
-    void printBookInformation() {
+    void printBookInformations() {
         String information = title + "; " + author + "; " + publishDate + "; " + pagesNumber
                 + "; " + publisher + "; " + isbn + ".";
         System.out.println(information);
--- src/Library.java
+++ src/Library.java
@@ -1,13 +1,15 @@
 public class Library {
     // This class will store information about books according to the Book template.
     public static void main(String[] args) {
-        final String appName = "Library v0.3";
+        final String appName = "Library v0.4";
 
         System.out.println(appName);
         System.out.println("Books available in the Library:");
         Book book_1 = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz", 1997, 330,
                 "Historia", "dbsjcbd2342");
-        book_1.printBookInformation();
+        Book book_2 = new Book("Potop", "Henryk Sienkiewicz", 2004);
+        book_1.printBookInformations();
+        book_2.printBookInformations();
 
     }
 }
