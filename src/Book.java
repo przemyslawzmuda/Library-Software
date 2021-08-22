ae37f41e73de93b6550dea986e376b02b6b8647e
przmnovitas <Not Set>
2021-08-22 21:14:16 +0200

    Add the Book class (template for creating new objects in the future).

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    // This class is a template for creating new book in the future.
+    String title;
+    String author;
+    int publishDate;
+    int pagesNumber;
+    String publisher;
+    String isbn;
+}
--- src/Library.java
+++ src/Library.java
@@ -1,20 +1,22 @@
 public class Library {
+    // This class will store information about books according to the Book template.
     public static void main(String[] args) {
-        final String appName = "Library v0.1";
+        final String appName = "Library v0.2";
 
-        String title = "";
-        String author = "";
-        int publishDate = 0;
-        int pagesNumber = 0;
-        String publisher = "";
-        String isbn = "";
+        Book book_1 = new Book();
+        book_1.title = "";
+        book_1.author = "";
+        book_1.publishDate = 0;
+        book_1.pagesNumber = 0;
+        book_1.publisher = "";
+        book_1.isbn = "";
 
         System.out.println(appName);
         System.out.println("Books available in the Library:");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(publishDate);
-        System.out.println(pagesNumber);
-        System.out.println(isbn);
+        System.out.println(book_1.title);
+        System.out.println(book_1.author);
+        System.out.println(book_1.publishDate);
+        System.out.println(book_1.pagesNumber);
+        System.out.println(book_1.isbn);
     }
 }
