f6fe7d9330e42fb01a1bf66de297e8ae81978b4a
przmnovitas <Not Set>
2021-08-23 09:22:09 +0200

    Declare books array to store books in the array.

--- src/librarySoftware/app/Library.java
+++ src/librarySoftware/app/Library.java
@@ -5,15 +5,17 @@ import librarySoftware.model.Book;
 public class Library {
     // This class will store information about books according to the Book template.
     public static void main(String[] args) {
-        final String appName = "Library v0.5";
+        final String appName = "Library v0.7";
 
+        Book[] booksArray = new Book[2000];
+
+        booksArray[0] = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz", 1997, 330,
+                "Historia", "dbsjcbd2342");
+        booksArray[1] = new Book("Potop", "Henryk Sienkiewicz", 2004);
         System.out.println(appName);
         System.out.println("Books available in the Library:");
-        Book book_1 = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz", 1997, 330,
-                "Historia", "dbsjcbd2342");
-        Book book_2 = new Book("Potop", "Henryk Sienkiewicz", 2004);
-        book_1.printBookInformations();
-        book_2.printBookInformations();
+        booksArray[0].printBookInformations();
+        booksArray[1].printBookInformations();
 
     }
 }
