c2b06ac649b1bd15ee0242c407bcfae99ea9ade5
przmnovitas <Not Set>
2021-08-23 10:42:28 +0200

    Add inputInt() function in the DataInput class.

--- src/librarySoftware/io/DataInput.java
+++ src/librarySoftware/io/DataInput.java
@@ -6,8 +6,15 @@ import java.util.Scanner;
 public class DataInput {
     /*
     This class will allow to create new objects according to the data entered by a user.
+    It should be only one place to perform interaction with the users.
      */
     private Scanner input = new Scanner(System.in); // create an input object to read data from a user as a class's field
+
+    public int inputInt() {
+        int number = input.nextInt();
+        input.nextLine(); // consume a whitespace from an enter key
+        return number;
+    }
 
     public Book readAndCreateBookObject() {
         System.out.println("Title:");
@@ -15,11 +22,9 @@ public class DataInput {
         System.out.println("Author:");
         String authorBook = input.nextLine();
         System.out.println("Publish date:");
-        int publishDateBook = input.nextInt();
-        input.nextLine(); // consume a whitespace from a key enter
+        int publishDateBook = inputInt();
         System.out.println("Pages number:");
-        int pagesNumberBook = input.nextInt();
-        input.nextLine();
+        int pagesNumberBook = inputInt();
         System.out.println("Publisher:");
         String publisherBook = input.nextLine();
         System.out.println("ISBN:");
