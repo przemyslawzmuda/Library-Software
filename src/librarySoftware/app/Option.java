f7d8ed41b62a50fd23f577b6d4e34a48e08ea033
przmnovitas <przm@novitas.com.pl>
2021-08-27 12:52:47 +0200

    Create Option enum type to display available options for the users.

---
+++ src/librarySoftware/app/Option.java
@@ -0,0 +1,37 @@
+package librarySoftware.app;
+
+enum Option {
+    // if no default constructor, pass the parameters
+    EXIT(0, "Exit."),
+    INSERT_BOOK(1, "Add a new magazine."),
+    INSERT_MAGAZINE(2, "Add a new magazine."),
+    PRINT_BOOKS_FROM_LIBRARY(3, "Display available books."),
+    PRINT_MAGAZINES_FROM_LIBRARY(4, "Display available magazines.");
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    // add getter in order to read the values of the value and description fields
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // override toString() method to display available options
+    @Override
+    public String toString() {
+        return value + " - " + description;
+    }
+
+    static Option createFromInt(int option) {
+        return Option.values()[option]; // returns an array with the all values in the corresponding enum type
+    }
+}
